27fd885366318c20c5be7450a7dec9427fa1c161
MdAshiquerRahman <t1ramd00@students.oamk.fi>
2025-04-09 12:59:21 +0300

    resolve conflict

--- users/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .api_views import UserProfileUpdateView
-
-urlpatterns = [
-    path('update/', UserProfileUpdateView.as_view(), name='api_update_profile'),
-]
--- videos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Video, VideoComment
-
-@admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'uploaded_by', 'uploaded_at')
-    search_fields = ('title', 'uploaded_by__username')
-
-admin.site.register(VideoComment)
