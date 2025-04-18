2800804622d5f1c18f7d8328725ad747032e9903
MdAshiquerRahman <92602508+MdAshiquerRahman@users.noreply.github.com>
2025-04-18 21:00:55 +0300

    Delete Backend/recipytutor directory

--- Backend/recipytutor/images/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ImagePost, ImageComment
-
-@admin.register(ImagePost)
-class ImagePostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'uploaded_by', 'uploaded_at')
-    search_fields = ('title', 'uploaded_by__username')
-
-admin.site.register(ImageComment)
--- Backend/recipytutor/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class MyUser(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
